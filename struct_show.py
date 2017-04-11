f3a22eed5e042a5c43486d291d13ec5e4af2832d
iaalm <iaalmsimon@gmail.com>
2017-04-11 21:42:16 +0800

    FIX:

--- struct_show.py
+++ struct_show.py
@@ -20,9 +20,8 @@ fixed_positions = {}
 for node in net.G.nodes():
     if isinstance(node, inputLayer):
         fixed_positions[node] = (0, node.layer_id)
-        print(node, node.layer_id)
     if isinstance(node, outputLayer):
-        fixed_positions[node] = (2, node.layer_id)
+        fixed_positions[node] = (1, node.layer_id)
 
 fixed_nodes = fixed_positions.keys()
 pos = nx.spring_layout(net.G, pos=fixed_positions, fixed=fixed_nodes)
