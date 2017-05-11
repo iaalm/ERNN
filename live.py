eb1c58713fb1de60caf4e341b6409571f961dc80
iaalm <iaalmsimon@gmail.com>
2017-05-11 09:15:05 +0800

    noloop live

--- live.py
+++ live.py
@@ -8,28 +8,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument("dir", metavar='dir', help="model dirs")
 args = parser.parse_args()
 
-while True:
-    fig = plt.figure()
-    result = []
-    for sub in ['live', 'dead']:
-        d = os.listdir(os.path.join(args.dir, sub))
-        for i in d:
-            with open(os.path.join(args.dir, sub, i, 'cell.lua')) as fd:
-                performance = fd.readline().split()[1].strip()
-                try:
-                    performance = float(performance)
-                    if performance < 0:
-                        continue
-                    result.append((int(i), performance))
-                except:
-                    pass
-                    # print(i, 'error')
-    result = sorted(result, key=lambda x: x[0])
-    plt.plot([i[1] for i in result], '.')
-    print(max([i[1] for i in result]))
-    print(sorted(result, key=lambda x: x[1])[-1])
+result = []
+for sub in ['live', 'dead']:
+    d = os.listdir(os.path.join(args.dir, sub))
+    for i in d:
+        with open(os.path.join(args.dir, sub, i, 'cell.lua')) as fd:
+            performance = fd.readline().split()[1].strip()
+            try:
+                performance = float(performance)
+                if performance < 0:
+                    continue
+                result.append((int(i), performance))
+            except:
+                pass
+                # print(i, 'error')
+result = sorted(result, key=lambda x: x[0])
+print(max([i[1] for i in result]))
+print(sorted(result, key=lambda x: x[1])[-1])
 
-    timer = fig.canvas.new_timer(interval=60000)
-    timer.add_callback(plt.close)
-    timer.start()
-    plt.show()
+plt.plot([i[1] for i in result], '.')
+plt.show()
